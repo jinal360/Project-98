319b7e1d4b5da2771b9b538d988353f9acd61772
jinal360 <73110780+jinal360@users.noreply.github.com>
2021-06-06 21:14:34 +0530

    Add files via upload

---
+++ SwapFile.py
@@ -0,0 +1,14 @@
+def swapFileData():
+
+ with open ('sample1', 'r') as a:
+    data_a = a.read()
+
+ with open ('sample2', 'r') as b:
+    data_b = b.read()
+
+with open ('sample1', 'w') as a:
+ a.write(data_b)
+
+ with open ('sample2', 'w') as b:
+  b.write(data_a)
+    
